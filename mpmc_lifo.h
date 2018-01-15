bf5dcafec0dcc5291fffd3ec347d26a60c8df28c
wang-bin <wbsecg1@gmail.com>
2018-01-15 22:01:12 +0800

    add mpmc lifo

---
+++ mpmc_lifo.h
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2017-2018 WangBin <wbsecg1 at gmail.com>
+ * MIT License
+ * Lock Free MPSC FIFO
+ * https://github.com/wang-bin/lockless
+ */
+#pragma once
+#include <atomic>
+#include <utility>
+
+#define MPMC_LIFO_RAW_NEXT_PTR 1
+
+template<typename T>
+class mpmc_lifo {
+public:
+    ~mpmc_lifo() {
+        clear();
+    }
+
+    void clear() { while(pop()) {}} // in consumer thread
+
+    template<typename... Args>
+    void emplace(Args&&... args) {
+        node *n = new node{std::forward<Args>(args)...};
+#if MPMC_LIFO_RAW_NEXT_PTR
+        n->next = io_.load();
+        while (!io_.compare_exchange_weak(n->next, n)) {}
+#else
+        node* t = io_.exchange(n, std::memory_order_acq_rel);
+        n->next.store(t, std::memory_order_release);
+#endif
+    }
+
+    void push(T&& v) {
+        node *n = new node{std::move(v)};
+        n->next = io_.load();
+        while (!io_.compare_exchange_weak(n->next, n)) {}
+    }
+
+    bool pop(T* v = nullptr) {
+        popping_++;
+        node* out = io_.load(); // pop() in another thread & out can be null
+        while (out && !io_.compare_exchange_weak(out, out->next)) {}
+        if (!out) // became empty in another thead pop()
+            return false;
+        if (v)
+            *v = std::move(out->v);
+        try_delete(out);
+        return true;
+    }
+private:
+    struct node {
+        T v;
+        node* next;
+    };
+
+    void delete_pendding(node* n) {
+        while (n) {
+            node *next = n->next;
+            delete n;
+            n = next;
+        }
+    }
+
+    void try_delete(node* n) {
+        if (popping_ == 1) {
+            node* ns = pendding_delete_.exchange(nullptr);
+            if (!--popping_) { // safe, another thread run into pop() now will not get a deleting out
+                delete_pendding(ns);
+            } else if (ns) { // 3 threads, t1 in try_delete before exchange(), t2  t3 just load() in pop() and get the same out, t3 finishes pop() first, t1 exchange and get t3 popped node, now popping_ is 2, if delete --popping>0, t2 later accesses invalid out 
+                delete_later(ns); // can not delete (delete later appended reading node, so considering 3 threads is enough)
+            }
+            delete n;
+        } else {
+            delete_later(n, n);
+            popping_--;
+        }
+    }
+
+    void delete_later(node* begin, node* end) {
+        end->next = pendding_delete_;
+        while (!pendding_delete_.compare_exchange_weak(end->next, begin)) {}
+    }
+    void delete_later(node* n) {
+        node* end = n;
+        while (node* const next = end->next) {
+            end = next;
+        }
+        delete_later(n, end);
+    }
+
+    std::atomic<node*> io_;
+    std::atomic<node*> pendding_delete_;
+    std::atomic<int> popping_;
+};
