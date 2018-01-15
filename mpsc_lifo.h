936db665dc3f8d8cc94d5b579544b6e6fe0aaeaa
wang-bin <wbsecg1@gmail.com>
2018-01-15 21:50:53 +0800

    lifo: must use raw next ptr
    unlike fifo, if next is atomic, can not tell whether null out->next means empty or unfinished push

--- mpsc_lifo.h
+++ mpsc_lifo.h
@@ -8,8 +8,6 @@
 #include <atomic>
 #include <utility>
 
-#define MPSC_LIFO_RAW_NEXT_PTR 0
-
 template<typename T>
 class mpsc_lifo {
 public:
@@ -22,24 +20,14 @@ public:
     template<typename... Args>
     void emplace(Args&&... args) {
         node *n = new node{std::forward<Args>(args)...};
-#if MPSC_LIFO_RAW_NEXT_PTR
         n->next = io_.load();
         while (!io_.compare_exchange_weak(n->next, n)) {}
-#else
-        node* t = io_.exchange(n, std::memory_order_acq_rel);
-        n->next.store(t, std::memory_order_release);
-#endif
     }
 
     void push(T&& v) {
         node *n = new node{std::move(v)};
-#if MPSC_LIFO_RAW_NEXT_PTR
         n->next = io_.load(); // next can be a raw ptr
         while (!io_.compare_exchange_weak(n->next, n)) {}
-#else
-        node* t = io_.exchange(n, std::memory_order_acq_rel);
-        n->next.store(t, std::memory_order_release);
-#endif
     }
 
     bool pop(T* v = nullptr) {
@@ -47,16 +35,7 @@ public:
         if (!out)
             return false;
         // io_ can be modified in push(), so compare is required
-#if MPSC_LIFO_RAW_NEXT_PTR
         while (!io_.compare_exchange_weak(out, out->next)) {}
-#else
-        node *n = nullptr;
-        do {
-            n = out->next.load(std::memory_order_relaxed);
-            if (!n) // next.store() may be not executed
-                return false;
-        } while (!io_.compare_exchange_weak(out, n));
-#endif
         if (v)
             *v = std::move(out->v);
         delete out;
@@ -65,11 +44,7 @@ public:
 private:
     struct node {
         T v;
-#if MPSC_LIFO_RAW_NEXT_PTR
         node* next;
-#else
-        std::atomic<node*> next;
-#endif
     };
 
     std::atomic<node*> io_;
